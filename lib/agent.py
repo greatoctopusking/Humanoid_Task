01b0937d879de6a0cb38acf20eddf38c12b28aa3
greatoctopusking <great_octopus_king@outlook.com>
2026-06-13 23:28:10 +0800

    Update agent.py

--- lib/agent.py
+++ lib/agent.py
@@ -42,7 +42,7 @@ class SACAgent(nn.Module):
         self.critic_1_target.load_state_dict(self.critic_1.state_dict())
         self.critic_2_target.load_state_dict(self.critic_2.state_dict())
 
-        self.alpha = 0.2
+        self.alpha = 0.5
 
         self._init_weights()
 
